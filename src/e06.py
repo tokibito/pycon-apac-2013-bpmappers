dc4bd9ce3e44a3b88e6d0be285b26dfcacab4b10
Shinya Okano <tokibito@gmail.com>
2013-09-15 10:23:33 +0900

    e06

---
+++ src/e06.py
@@ -0,0 +1,29 @@
+# 例6. bpmappersを使う場合(1対N)
+import json
+from bpmappers import Mapper, RawField, DelegateField
+
+# データモデル
+class Book(object):
+    def __init__(self, title, price, author):
+        self.title = title
+        self.price = price
+        self.author = author
+
+class Author(object):
+    def __init__(self, name):
+        self.name = name
+
+# マッピング用クラス
+class AuthorMapper(Mapper):
+    name = RawField()
+
+class BookMapper(Mapper):
+    title = RawField()
+    price = RawField()
+    author = DelegateField(AuthorMapper)
+
+author = Author("tokibito")
+book = Book("Spam", 500, author)
+# マッピングとJSON変換(2種類)
+print("author:", json.dumps(AuthorMapper(author).as_dict()))
+print("book:", json.dumps(BookMapper(book).as_dict()))
