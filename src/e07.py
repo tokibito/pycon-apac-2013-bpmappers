b60f3fb84933e3ecaf26aa92eab88ccd98eba3b7
Shinya Okano <tokibito@gmail.com>
2013-09-15 11:08:00 +0900

    07まで

---
+++ src/e07.py
@@ -0,0 +1,36 @@
+# 例7. bpmappersを使う場合(1対N) 名前だけ返すものを追加
+import json
+from bpmappers import Mapper, RawField, DelegateField
+
+# データモデル
+class Book(object):
+    def __init__(self, title, price, author):
+        self.title = title
+        self.price = price
+        self.author = author
+
+class Author(object):
+    def __init__(self, name, company):
+        self.name = name
+        self.company = company
+
+# マッピング用クラス
+class AuthorNameOnlyMapper(Mapper):
+    name = RawField()
+
+class AuthorMapper(AuthorNameOnlyMapper):
+    company = RawField()
+
+class BookNameOnlyMapper(Mapper):
+    title = RawField()
+    author = DelegateField(AuthorNameOnlyMapper)
+
+class BookMapper(BookNameOnlyMapper):
+    author = DelegateField(AuthorMapper)
+
+author = Author("tokibito", "BeProud")
+book = Book("Spam", 500, author)
+# マッピングとJSON変換(2種類)
+print("author:", json.dumps(AuthorMapper(author).as_dict()))
+print("book:", json.dumps(BookMapper(book).as_dict()))
+print("book(name only):", json.dumps(BookNameOnlyMapper(book).as_dict()))
