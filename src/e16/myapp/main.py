c48411d15f8fc279d93f01d2c3c42d6e33c6f6d7
Shinya Okano <tokibito@gmail.com>
2013-09-15 13:24:07 +0900

    例16

---
+++ src/e16/myapp/main.py
@@ -0,0 +1,21 @@
+import json
+from myapp.models import Author, Book
+from bpmappers.djangomodel import ModelMapper
+from bpmappers import DelegateField
+
+class AuthorMapper(ModelMapper):
+    class Meta:
+        model = Author
+        exclude = ['id']
+
+class BookMapper(ModelMapper):
+    author = DelegateField(AuthorMapper)
+    class Meta:
+        model = Book
+        exclude = ['id']
+
+author = Author(name="tokibito", company="BeProud")
+book = Book(title="Spam", price=500, author=author)
+# マッピングとJSON変換
+print("author:", json.dumps(AuthorMapper(author).as_dict(), indent=2))
+print("book:", json.dumps(BookMapper(book).as_dict(), indent=2))
